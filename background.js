463346c285136dd45ce7beb17dd13f57dcd1a6e4
Tengiz Ibrayev <tengizibrayev@Tengizs-MacBook-Pro.local>
2023-05-19 19:43:30 +0600

    Works one way (Cyrillic to Latin)

---
+++ background.js
@@ -0,0 +1,62 @@
+// Receive messages from content scripts
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message && message.message === 'Content script loaded') {
+    console.log('Content script loaded');
+  }
+  if (message && message.message === 'Button activated'){
+    console.log('Button activated')
+  }
+  if (message && message.message === 'Button desactivated'){
+    console.log('Button desactivated')
+  }
+});
+
+// Function to handle button click
+function handleButtonClick(tab) {
+  chrome.storage.local.get('buttonState', function(data) {
+    var buttonState = data.buttonState;
+
+    // Toggle the button state
+    var newButtonState = (buttonState === 'on') ? 'off' : 'on';
+    chrome.storage.local.set({ 'buttonState': newButtonState });
+
+    // Send a message to the content script
+    chrome.tabs.sendMessage(tab.id, { message: 'Button state changed', state: newButtonState });
+    console.log('Button state changed message sent ' + newButtonState);
+
+    // Update the badge text
+    // chrome.action.setBadgeText({ tabId: tab.id, text: newButtonState });
+    chrome.action.setIcon({path : 'icon_' + newButtonState + '.png'});
+  });
+}
+
+
+// Add click listener to the extension button
+chrome.action.onClicked.addListener(function(tab) {
+  // Query the tab to check if it exists
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    handleButtonClick(tabs[0]);
+  });
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    if (tabs.length > 0 && tabs[0].id === tabId) {      
+      chrome.storage.local.get('buttonState', function(data) {
+        var buttonState = data.buttonState;
+        if (buttonState === 'on') {
+          // Send a message to the content script
+          chrome.tabs.sendMessage(tab.id, { message: 'Page reloaded', state: buttonState }, function(response) {
+            if (chrome.runtime.lastError) {
+              console.error(chrome.runtime.lastError.message);
+              return;
+            }
+            console.log('Message sent to content script:', response);
+          });
+        }
+      });
+    }
+  });
+});
+
+
---
+++ content.js
@@ -0,0 +1,119 @@
+// Store the initial state of the button
+var initialButtonState = 'off';
+var originalTextContent; // Variable to store the original content
+
+// Receive messages from the background script
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message && message.message === 'Button state changed') {
+    var buttonState = message.state;
+    if (buttonState === 'on') {
+      chrome.runtime.sendMessage({ message: 'Button activated, script executed' });
+      changeTextContent();
+    } else if (buttonState === 'off') {
+      resetTextContent();
+    }
+  } else if (message && message.message === 'Page reloaded') {
+    chrome.storage.local.get('buttonState', function(data) {
+      var buttonState = data.buttonState;
+      if (buttonState === 'on') {
+        chrome.runtime.sendMessage({ message: 'Button activated, script executed' });
+        changeTextContent();
+      } else if (buttonState === 'off') {
+        resetTextContent();
+      }
+    });
+  }
+
+  // Send a response back to the sender
+  sendResponse({ success: true });
+});
+
+
+
+// Notify the background script that the content script has been loaded
+chrome.runtime.sendMessage({ message: 'Content script loaded' });
+
+// Function to reset the text content to the original state
+function resetTextContent() {
+  if (originalTextContent) {
+    // Restore the original content
+    var currentTextContent = document.body.textContent;
+    var modifiedContent = currentTextContent.replace(originalTextContent, '');
+    document.body.textContent = modifiedContent;
+  }
+}
+
+
+function changeTextContent() {
+  // Get all text nodes in the document
+  const textNodes = document.evaluate("//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+  
+  for (let i = 0; i < textNodes.snapshotLength; i++) {
+    const textNode = textNodes.snapshotItem(i);
+    const originalText = textNode.textContent;
+    const newText = transliterateKazakh(originalText);
+    
+    if (originalText !== newText) {
+      textNode.textContent = newText;
+    }
+  }
+}
+  
+const cyrillicToLatin = {
+  'Һ': 'h', 'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ж': 'j', 'з': 'z',
+  'й': 'y', 'и': 'ï', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's',
+  'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 's', 'ч': 'sh', 'ш': 'sh', 'щ': 'sh', 'ъ': '', 'ы': 'y',
+  'ь': '', 'э': 'e', 'ю': 'u', 'я': 'a', 'ё': 'o', 'і': 'i', 'ғ': 'gh', 'қ': 'q', 'ң': 'nh', 'ү': 'ü',
+  'ұ': 'w', 'ә': 'ä', 'ө': 'ö'
+};
+
+Object.keys(cyrillicToLatin).forEach(function (key) {
+  cyrillicToLatin[key.charAt(0).toUpperCase() + key.slice(1)] = cyrillicToLatin[key].charAt(0).toUpperCase() + cyrillicToLatin[key].slice(1);
+});
+
+const vowels = 'аәеёиоөуүұыэюя';
+
+function transliterateKazakh(text) {
+    let previousLetter = null;
+    let newtext = '';
+  
+    for (let i = 0; i < text.length; i++) {
+      const character = text[i];
+      let result = character;
+  
+      if (cyrillicToLatin.hasOwnProperty(character)) {
+        if (isFitToSplitDiphthong(character, previousLetter)) {
+          result = splitDiphthong(character);
+        } else {
+          result = cyrillicToLatin[character];
+        }
+      }
+  
+      newtext += result;
+      previousLetter = character;
+    }
+  
+    return newtext;
+}
+  
+function isFitToSplitDiphthong(currentLetter, previousLetter) {
+    if (!isDiphthong(currentLetter)) {
+      return false;
+    }
+    if (previousLetter === null) {
+      return true;
+    }
+    if (previousLetter.toLowerCase() === 'и') {
+      return false;
+    }
+    return (vowels + 'ьъ ').includes(previousLetter.toLowerCase());
+}
+  
+function isDiphthong(letter) {
+    return 'еёюяЕЁЮЯ'.includes(letter);
+}
+  
+function splitDiphthong(character) {
+  var firstLetter = character.toUpperCase() === character ? 'Й' : 'й';
+  return cyrillicToLatin[firstLetter] + cyrillicToLatin[character.toLowerCase()];
+}
